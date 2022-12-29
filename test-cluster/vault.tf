78503314caa44f9413ce493e212948a20df6b931
steveizzle <stefan.riembauer@gmail.com>
2022-12-29 23:18:43 +0100

    path for renovate

--- test-cluster/vault.tf
+++ test-cluster/vault.tf
@@ -1,4 +1,4 @@
 module "vault_module" {
-  source = "git::ssh://git@github.com/steveizzle/vault-tfmodule.git?ref=v0.0.7-rc7"
+  source = "git::ssh://git@github.com/steveizzle/vault-tfmodule.git?ref=v0.0.7-rc6"
   name = var.vault_name
 }
