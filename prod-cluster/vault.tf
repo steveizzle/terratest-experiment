d6e9ec9355af8af7768427a781f11f0152ae5b87
steveizzle <stefan.riembauer@gmail.com>
2022-12-29 23:41:41 +0100

    path for renovate

--- prod-cluster/vault.tf
+++ prod-cluster/vault.tf
@@ -1,4 +1,4 @@
 module "vault_module" {
-  source = "git::ssh://git@github.com/steveizzle/vault-tfmodule.git?ref=v0.0.7"
+  source = "git::ssh://git@github.com/steveizzle/vault-tfmodule.git?ref=v0.0.6"
   name = "vault-prod"
 }
