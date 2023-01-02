b1cf6842a6306970e96fe68012c75ecf734c6560
steveizzle <stefan.riembauer@gmail.com>
2023-01-02 17:01:07 +0100

    Makefile and test script
    Signed-off-by: steveizzle <stefan.riembauer@gmail.com>

--- test-cluster-rancher/vault.tf
+++ test-cluster-rancher/vault.tf
@@ -1,3 +1,7 @@
 module "vault_module" {
-  source = "../vault-tfmodule"
+  source = "git::ssh://git@github.com/steveizzle/terratest-experiment.git//vault-tfmodule?ref=main"
+}
+module "vault_module_2" {
+  source = "git::ssh://git@github.com/steveizzle/terratest-experiment.git//vault-tfmodule?ref=main"
+  name = "vault-2"
 }
