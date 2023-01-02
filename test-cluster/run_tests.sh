265b0d064f57dd34c48297da31245067c5c5d178
steveizzle <stefan.riembauer@gmail.com>
2023-01-02 17:01:25 +0100

    Added terratest_log_parser to test script
    Signed-off-by: steveizzle <stefan.riembauer@gmail.com>

--- test-cluster/run_tests.sh
+++ test-cluster/run_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+go install github.com/gruntwork-io/terratest/cmd/terratest_log_parser@latest
+
 for module in $(ls .terraform/modules/); do 
-	make tests -C .terraform/modules/$module; 
+	make tests -C .terraform/modules/${module} | tee test_output_${module}.log
+	terratest_log_parser -testlog test_output_${module}.log -outputdir ./test-reports/${module}/
 done
 
