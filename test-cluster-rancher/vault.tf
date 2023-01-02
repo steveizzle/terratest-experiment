317ac37815c8e60f266a6fd598e004fedba00e46
steveizzle <stefan.riembauer@gmail.com>
2023-01-02 17:01:08 +0100

    workflow and module url
    Signed-off-by: steveizzle <stefan.riembauer@gmail.com>

--- test-cluster-rancher/vault.tf
+++ test-cluster-rancher/vault.tf
@@ -1,4 +1,4 @@
 module "vault_module" {
-  source = "git::ssh://git@github.com/steveizzle/terratest-experiment.git//vault-tfmodule?ref=main"
+  source = "git::ssh://git@github.com/steveizzle/vault-tfmodule.git?ref=v0.0.1"
   name = var.vault_name
 }
