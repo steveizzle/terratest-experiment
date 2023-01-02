c2a2d619023b1905dbdd7b8a0c61663ff25d429b
steveizzle <stefan.riembauer@gmail.com>
2023-01-02 17:01:26 +0100

    Flux Sources and test script
    Signed-off-by: steveizzle <stefan.riembauer@gmail.com>

--- test-cluster/run_tests.sh
+++ test-cluster/run_tests.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+INTEGRATION_TESTS="vault_module"
+
+echo "Installing dependencies"
 go install github.com/gruntwork-io/terratest/cmd/terratest_log_parser@latest
 
-for module in $(ls .terraform/modules/); do 
+echo "Running Tests"
+
+for module in $INTEGRATION_TESTS; do 
+	echo "Run tests for Module ${module}..."
 	make tests -C .terraform/modules/${module} | tee test_output_${module}.log
 	terratest_log_parser -testlog test_output_${module}.log -outputdir ./test-reports/${module}/
 done
