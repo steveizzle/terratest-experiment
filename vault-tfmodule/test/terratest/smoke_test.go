3dfcb2feb05b108dcb97870b60f35143e427c691
steveizzle <stefan.riembauer@gmail.com>
2023-01-02 17:01:08 +0100

    Variable and specifying tf dir as env var
    Signed-off-by: steveizzle <stefan.riembauer@gmail.com>

--- vault-tfmodule/test/terratest/smoke_test.go
+++ vault-tfmodule/test/terratest/smoke_test.go
@@ -1,25 +1,38 @@
 package test
 
 import (
+	"fmt"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
 func TestTerraformHelloWorldExample(t *testing.T) {
 	// Construct the terraform options with default retryable errors to handle the most common
 	// retryable errors in terraform testing.
-	// terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-	// 	// Set the path to the Terraform code that will be tested.
-	// 	TerraformDir: "../..",
-	// })
+	terraformDir := os.Getenv("TF_DIR")
+	if terraformDir == "" {
+		terraformDir = "../.."
+	}
+
+	fmt.Printf("Applying %s\n", terraformDir)
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: terraformDir,
+		Vars: map[string]interface{}{
+			"vault_name": "vault-test",
+		},
+	})
 
 	// Clean up resources with "terraform destroy" at the end of the test.
 	// defer terraform.Destroy(t, terraformOptions)
 
 	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
-	// terraform.InitAndApply(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
 
 	// Run `terraform output` to get the values of output variables and check they have the expected values.
 	// output := terraform.Output(t, terraformOptions, "hello_world")
